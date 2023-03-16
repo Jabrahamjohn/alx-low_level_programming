b7e0a229fc0a55930dd84facb5472f7168b4a3dc
AJ <aj@AJ.Jabrahamjohns@gmail.com>
2023-03-17 01:51:34 +0300

    c is fun

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main -prints the required info
+ *
+ * Description: using the main function
+ * compiles without warning when -waal gcc option is used
+ * Return: 1
+*/
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"Dora Korpar, 2015-10-19\n");
+return (1);
+
+}
