3f374ef787ab02a6dbb263e5497d42911361daa0
AJ <aj@AJ.Jabrahamjohns@gmail.com>
2023-03-17 03:01:00 +0300

    c is fun

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
  *main -prints the required info
@@ -10,7 +9,7 @@
 */
 int main(void)
 {
-	fwrite(2 "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	write(2 "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
 return (1);
 
 }
