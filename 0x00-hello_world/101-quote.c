927d6a6c3911d272eb53975a0ec2b5aebe80c792
AJ <aj@AJ.Jabrahamjohns@gmail.com>
2023-03-17 03:17:46 +0300

    c is fun

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -9,7 +10,7 @@
 */
 int main(void)
 {
-	write(2 "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 
 return (1);
 
