e11c55fd34f375057cd56fb2f3d2dbe7986276b7
AJ <aj@AJ.Jabrahamjohns@gmail.com>
2023-03-18 02:51:22 +0300

    c variables and if statements

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,26 +8,25 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (j = i; j < 100; j++)
+		for (j = i; j <= 99; j++)
 		{
 			int a = i / 10;
 			int b = i % 10;
 			int c = j / 10;
 			int d = j % 10;
 
-			if (i == j)
-				continue;
-			if (c < a || (c == a && d <= b))
-				continue;
 			putchar('0' + a);
 			putchar('0' + b);
 			putchar(' ');
 			putchar('0' + c);
 			putchar('0' + d);
+			if (i != 99 || j != 99)
+			{
 			putchar(',');
 			putchar(' ');
+			}
 		}
 	}
 putchar('\n');
