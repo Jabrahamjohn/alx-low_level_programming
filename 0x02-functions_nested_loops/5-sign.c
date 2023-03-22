9031cd5cfd621952941156fcd52069e95daea890
Abraham John <101580587+Jabrahamjohn@users.noreply.github.com>
2023-03-22 03:20:34 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		putchar('+');
 		return (1);
 	}
 	if (n == 0)
 	{
-		_putchar('0');
+		putchar('0');
 		return (0);
 	}
 	if (n < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		return (-1);
 	}
 
