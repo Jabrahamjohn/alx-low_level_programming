e16adc02301bf8ff8773ac37c61a38605a690ed0
Abraham John <101580587+Jabrahamjohn@users.noreply.github.com>
2023-03-22 04:11:20 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,31 +4,32 @@
  * main-prins all natural numbers from n to 98
  * return: returns he numbers
  **/
-int main(void) print_to_98(int n)
+int main(void)
 {
-    if (n > 98)
-    {
+	int n = print_to_98;
+	if (n > 98)
+	{
         for (int i = n; i >= 98; i--)
-	{
-            printf("%d", i);
-            if (i != 98)
-	    {
+		{
+		printf("%d", i);
+		if (i != 98)
+		{
                 putchar(',');
                 putchar(' ');
-            }
-        }
-    }
-    else
-    {
+		}
+		}
+	}
+    	else
+	{
         for (int i = n;	i <= 98; i++)
-	{
-            printf("%d", i);
-            if (i != 98)
-	    {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
+		{
+		printf("%d", i);
+		if (i != 98)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		}
+	}
+	putchar('\n');
 }
