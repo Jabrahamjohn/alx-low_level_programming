2621a62b02278fff3a6f0386406208d1a4cefbcd
Abraham John <101580587+Jabrahamjohn@users.noreply.github.com>
2023-03-22 03:32:28 +0300

    Update 5-main.c

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -7,27 +7,8 @@
  */
 int main(void)
 {
-	int r;
-
-	r = print_sign(98);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	r = print_sign(0);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	r = print_sign(0xff);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	r = print_sign(-1);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	return (0);
-}
+	int n = -5;
+	int sign = print_sign(n);
+	printf("%d\n", sign);
+	return 0;
+	}
