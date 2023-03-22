7cc2e70d5e5e3deeb54be403805d7fc29b3fefdb
Abraham John <101580587+Jabrahamjohn@users.noreply.github.com>
2023-03-22 03:40:23 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  *print_last_digit - check the code for Holberton School students.
@@ -8,12 +9,9 @@
  */
 int print_last_digit(int last)
 {
-	last = last % 10;
-	if (last < 0)
+	last = abs (last) % 10;
 	{
-		putchar((last * -1) '0');
-		return (last * -1);
-	}
-	putchar((last) '0');
-	return (last);
+		putchar("%d", last);
+		return (last);
+	
 }
