59aaccedff95ddc15ff459053edf59c47ccbbaf9
Abraham John <101580587+Jabrahamjohn@users.noreply.github.com>
2023-03-22 03:26:21 +0300

    Update 5-main.c

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include <stdio.h>
 
 /**
  * main - check the code for Holberton School students.
@@ -10,24 +10,24 @@ int main(void)
 	int r;
 
 	r = print_sign(98);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
+	putchar(',');
+	putchar(' ');
+	putchar(r + '0');
+	putchar('\n');
 	r = print_sign(0);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
+	putchar(',');
+	putchar(' ');
+	putchar(r + '0');
+	putchar('\n');
 	r = print_sign(0xff);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
+	putchar(',');
+	putchar(' ');
+	putchar(r + '0');
+	putchar('\n');
 	r = print_sign(-1);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
+	putchar(',');
+	putchar(' ');
+	putchar(r + '0');
+	putchar('\n');
 	return (0);
 }
