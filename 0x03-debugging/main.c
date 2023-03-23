d6f53209661fdd831e3da415868ad906f7e2bdd0
ABRAHAM JOHN <Jabrahamjohns@gmail.com>
2023-03-22 20:02:38 -0700

    c debugging

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -4,7 +4,6 @@
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-
 int main(void)
 {
         int i;
@@ -13,4 +12,5 @@ int main(void)
         positive_or_negative(i);
 
         return (0);
+}
 
