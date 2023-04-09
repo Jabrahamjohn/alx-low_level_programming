75b196cdbd27fed51eca416477ec4f81c4991901
Jabrahamjohn <jabrahamjohns@gmail.com>
2023-04-09 15:11:39 -0400

    arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,58 +1,84 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-/**
- * check_num - checks string that there are digit
- * @str: array str
- * main -checks for variables
- * Return: Always 0 (Success)
- */
-int check_num(char *str)
-{
-	/*Declaring variables*/
-	unsigned int count;
-
-	count = 0;
-	while (count < strlen(str)) /*count string*/
+	#include <stdlib.h>
+	#include <ctype.h>
+	#include <string.h>
+	/**
+	 * check_num - check - string there are digit
+	 * @str: array str
+	 *
+	 * Return: Always 0 (Success)
+	 */
+	int check_num(char *str)
 	{
-		if (!isdigit(str[count])) /*check if str there are digit*/
+		/*Declaring variables*/
+		unsigned int count;
+
+
+		count = 0;
+		while (count < strlen(str)) /*count string*/
+
+
 		{
-			return (0);
+			if (!isdigit(str[count])) /*check if str there are digit*/
+			{
+				return (0);
+			}
+
+
+			count++;
 		}
-		count++;
+		return (1);
 	}
-	return (1);
-}
-/**
- * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
- * Return: Always 0 (Success)
- */
-int main(int argc, char *argv[])
-{
-	/*Declaring variables*/
-	int count;
-	int str_to_int;
-	int sum = 0;
 
-	count = 1;
-	while (count < argc) /*Goes through the whole array*/
+
+	/**
+	 * main - Print the name of the program
+	 * @argc: Count arguments
+	 * @argv: Arguments
+	 *
+	 * Return: Always 0 (Success)
+	 */
+
+
+	int main(int argc, char *argv[])
+
+
 	{
-		if (check_num(argv[count]))
+
+
+		/*Declaring variables*/
+		int count;
+		int str_to_int;
+		int sum = 0;
+
+
+		count = 1;
+		while (count < argc) /*Goes through the whole array*/
 		{
-			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-			sum += str_to_int;
+			if (check_num(argv[count]))
+
+
+			{
+				str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+				sum += str_to_int;
+			}
+
+
+			/*Condition if one of the number contains symbols that are not digits*/
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+
+			count++;
 		}
-		/*Condition if one of the number contains symbols that are not digits*/
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
-		count++;
+
+
 		printf("%d\n", sum); /*print sum*/
+
+
 		return (0);
 	}
 
