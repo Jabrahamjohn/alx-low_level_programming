b23517b2378c88ec209a2c0e203e188a5bab8591
Jabrahamjohn <jabrahamjohns@gmail.com>
2023-05-02 05:30:00 +0300

    more lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,14 +14,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t num = 0;
 
 	while (h)
 	{
-		nodes++;
+		num++;
 		printf("%d\n", h->h);
 		h = h->next;
 	}
 
-	return (nodes);
+	return (num);
 }
