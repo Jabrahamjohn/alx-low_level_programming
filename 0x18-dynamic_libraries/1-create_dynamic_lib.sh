a9a6e03e6b2964de535e913f18f179b376f266d6
Jabrahamjohn <jabrahamjohns@gmail.com>
2023-08-23 00:13:49 +0300

    Without libraries what have we?  We have no past and no future

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
-gcc -fPIC -c *.c
-gcc -shared -o liball.so *.o
+
+# Compile all .c files in the current directory into a dynamic library
+
+# Gather all .c files in the current directory
+SOURCE_FILES=$(ls *.c 2>/dev/null)
+
+# Check if there are any .c files
+if [ -z "$SOURCE_FILES" ]; then
+    echo "No .c files found in the current directory."
+    exit 1
+fi
+
+# Compile the dynamic library
+gcc -shared -o liball.so -fPIC $SOURCE_FILES
+
+# Check if compilation was successful
+if [ $? -eq 0 ]; then
+    echo "Dynamic library liball.so created successfully."
+else
+    echo "Error creating dynamic library."
+fi
