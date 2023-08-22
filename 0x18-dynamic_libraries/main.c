9ab3195fe1f328f8134a58809b38505fa343ea5b
Jabrahamjohn <jabrahamjohns@gmail.com>
2023-08-22 21:15:41 +0300

    finally am trying the library of mine own

--- 0x18-dynamic_libraries/main.c
+++ 0x18-dynamic_libraries/main.c
@@ -1,60 +1,22 @@
 #include "main.h"
-#include <stdio.h>
-
-/**
- * main - check the code
- * 
- * Return: Always EXIT_SUCCESS.
-*/
-int _putchar(char c) {
-    return write(1, &c, 1);
-}
-
-int _islower(int c) {
-    return (c >= 'a' && c <= 'z');
-}
-
-int _isalpha(int c) {
-    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-}
-
-int _abs(int n) {
-    return (n < 0) ? -n : n;
-}
-
-int _isupper(int c) {
-    return (c >= 'A' && c <= 'Z');
-}
-
-int _isdigit(int c) {
-    return (c >= '0' && c <= '9');
-}
-
-int _strlen(char *s) {
-    int len = 0;
-    while (*s != '\0') {
-        len++;
-        s++;
-    }
-    return len;
-}
-
-void _puts(char *s) {
-    while (*s != '\0') {
-        _putchar(*s);
-        s++;
-    }
-}
-
-char *_strcpy(char *dest, char *src) {
-    char *dest_ptr = dest;
-    while (*src != '\0') {
-        *dest = *src;
-        dest++;
-        src++;
-    }
-    *dest = '\0';
-    return dest_ptr;
-}
-
-// Implement the rest of the functions similarly
+#include <stddef.h>
+int _putchar(char c){}
+int _islower(int c){}
+int _isalpha(int c){}
+int _abs(int n){}
+int _isupper(int c){}
+int _isdigit(int c){}
+int _strlen(char *s){}
+void _puts(char *s){}
+char *_strcpy(char *dest, char *src){}
+int _atoi(char *s){}
+char *_strcat(char *dest, char *src){}
+char *_strncat(char *dest, char *src, int n){}
+char *_strncpy(char *dest, char *src, int n){}
+int _strcmp(char *s1, char *s2){}
+char *_memset(char *s, char b, unsigned int n){}
+char *_memcpy(char *dest, char *src, unsigned int n){}
+char *_strchr(char *s, char c){}
+unsigned int _strspn(char *s, char *accept){}
+char *_strpbrk(char *s, char *accept){}
+char *_strstr(char *haystack, char *needle){}
