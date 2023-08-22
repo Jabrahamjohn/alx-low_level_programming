86fd93435b3a02f0ee4badf517161fd1624d92e2
Jabrahamjohn <jabrahamjohns@gmail.com>
2023-08-23 00:04:13 +0300

    libraries here they are

--- 0x18-dynamic_libraries/main.c
+++ 0x18-dynamic_libraries/main.c
@@ -1,22 +1,81 @@
-#include "main.h"
-#include <stddef.h>
-int _putchar(char c){}
-int _islower(int c){}
-int _isalpha(int c){}
-int _abs(int n){}
-int _isupper(int c){}
-int _isdigit(int c){}
-int _strlen(char *s){}
-void _puts(char *s){}
-char *_strcpy(char *dest, char *src){}
-int _atoi(char *s){}
-char *_strcat(char *dest, char *src){}
-char *_strncat(char *dest, char *src, int n){}
-char *_strncpy(char *dest, char *src, int n){}
-int _strcmp(char *s1, char *s2){}
-char *_memset(char *s, char b, unsigned int n){}
-char *_memcpy(char *dest, char *src, unsigned int n){}
-char *_strchr(char *s, char c){}
-unsigned int _strspn(char *s, char *accept){}
-char *_strpbrk(char *s, char *accept){}
-char *_strstr(char *haystack, char *needle){}
+#include <unistd.h>
+
+int _putchar(char c) {
+    return write(1, &c, 1);
+}
+
+int _islower(int c) {
+    // Implement _islower
+}
+
+int _isalpha(int c) {
+    // Implement _isalpha
+}
+
+int _abs(int n) {
+    // Implement _abs
+}
+
+int _isupper(int c) {
+    // Implement _isupper
+}
+
+int _isdigit(int c) {
+    // Implement _isdigit
+}
+
+int _strlen(char *s) {
+    // Implement _strlen
+}
+
+void _puts(char *s) {
+    // Implement _puts
+}
+
+char *_strcpy(char *dest, char *src) {
+    // Implement _strcpy
+}
+
+int _atoi(char *s) {
+    // Implement _atoi
+}
+
+char *_strcat(char *dest, char *src) {
+    // Implement _strcat
+}
+
+char *_strncat(char *dest, char *src, int n) {
+    // Implement _strncat
+}
+
+char *_strncpy(char *dest, char *src, int n) {
+    // Implement _strncpy
+}
+
+int _strcmp(char *s1, char *s2) {
+    // Implement _strcmp
+}
+
+char *_memset(char *s, char b, unsigned int n) {
+    // Implement _memset
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n) {
+    // Implement _memcpy
+}
+
+char *_strchr(char *s, char c) {
+    // Implement _strchr
+}
+
+unsigned int _strspn(char *s, char *accept) {
+    // Implement _strspn
+}
+
+char *_strpbrk(char *s, char *accept) {
+    // Implement _strpbrk
+}
+
+char *_strstr(char *haystack, char *needle) {
+    // Implement _strstr
+}
