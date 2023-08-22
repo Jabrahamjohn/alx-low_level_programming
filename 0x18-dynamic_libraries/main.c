e56f4910e29f5783812cbc4964d1999219745b74
Jabrahamjohn <jabrahamjohns@gmail.com>
2023-08-22 19:57:06 +0300

    i've reading within the libraries and it's tough

--- 0x18-dynamic_libraries/main.c
+++ 0x18-dynamic_libraries/main.c
@@ -6,8 +6,55 @@
  * 
  * Return: Always EXIT_SUCCESS.
 */
-int main(void)
-{
-    printf("%d\n", _strlen("My Dyn Lib"));
-    return (EXIT_SUCCESS);
+int _putchar(char c) {
+    return write(1, &c, 1);
 }
+
+int _islower(int c) {
+    return (c >= 'a' && c <= 'z');
+}
+
+int _isalpha(int c) {
+    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+int _abs(int n) {
+    return (n < 0) ? -n : n;
+}
+
+int _isupper(int c) {
+    return (c >= 'A' && c <= 'Z');
+}
+
+int _isdigit(int c) {
+    return (c >= '0' && c <= '9');
+}
+
+int _strlen(char *s) {
+    int len = 0;
+    while (*s != '\0') {
+        len++;
+        s++;
+    }
+    return len;
+}
+
+void _puts(char *s) {
+    while (*s != '\0') {
+        _putchar(*s);
+        s++;
+    }
+}
+
+char *_strcpy(char *dest, char *src) {
+    char *dest_ptr = dest;
+    while (*src != '\0') {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = '\0';
+    return dest_ptr;
+}
+
+// Implement the rest of the functions similarly
